bd6860f0aae79be41199a57ea2a52301216a88fd
Umed <umedmirsaidov61@gmail.com>
2023-10-05 21:48:58 +0500

    Added error and request

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,15 @@
             <div class="p-6 flex justify-center bg-white border-b border-gray-200">
                 <section>
                     <h1 class="text-4xl text-blue-800">Short you link</h1>
+                    @if (session('success_message'))
+                        {!! session('success_message') !!}
+                    @endif
                     <form method="POST" action="{{route('short.url')}}">
                         @csrf
                         <input class="border border-gray-300 rounded-lg" type="url" name="original_url">
+                        @error('original_url')
+                            <span class="text-red-400 m-2 p-2">{{$message}}</span>
+                        @enderror
                         <button class="m-2 px-6 py-2 bg-green-500 hover:bg-green-700 rounded-lg" type="submit">Short</button>
                     </form>
                 </section>
