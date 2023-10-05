87517de91cd26316c3a25aacabd62063385046a7
Umed <umedmirsaidov61@gmail.com>
2023-10-05 21:20:23 +0500

    Changed function

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShortUrl;
 use Illuminate\Http\Request;
 
 class ShortUrlController extends Controller
 {
     public function short(Request $request)
     {
-        dd($request);
+        if($request->original_url){
+            $new_url = ShortUrl::create([
+                'original_url' => $request->original_url
+            ]);
+            if($new_url){
+                $short_url = base_convert($new_url->id, 10,36);
+                $new_url->update([
+                    'short_url' => $short_url
+                    ]);
+                return back();
+            };
+        };
+        return back();
     }
 }
